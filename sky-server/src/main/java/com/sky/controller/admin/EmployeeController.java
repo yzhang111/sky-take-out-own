b7bed60e544c2966b453d155d4c55b83fd348f3f
yzhan111 <yzhan661206@gmail.com>
2025-01-01 17:21:44 +0900

    Enable or disable employee account function added

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -97,4 +97,12 @@ public class EmployeeController {
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用或禁用员工")
+    public Result startOrStop(@PathVariable("status") Integer status, Long id) {
+        log.info("启用或者禁用员工账号: {}, {}", status, id);
+        employeeService.startOrStop(status, id);
+        return Result.success();
+    }
 }
